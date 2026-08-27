317dc1e3a6e5ad5a2a050764155826e46236e012
Egor <egor.krushinov@aspose.com>
2026-08-27 17:57:29 +0600

    docs-owner: update QA docs sample

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,5 @@ import React from "react";
 import {Redirect} from "@docusaurus/router";
 
 export default function Home() {
-  return <Redirect to="/net/viewer/v26.5/getting-started/" />;
+  return <Redirect to="/net/viewer/v26.4.0/getting-started/" />;
 }
