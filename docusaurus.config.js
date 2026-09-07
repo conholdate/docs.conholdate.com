396cbcc8f1d66a8309a169237beab1c7bd0820bd
Egor <egor.krushinov@aspose.com>
2026-09-07 13:16:09 +0600

    Fix Java installation page rendering.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -114,7 +114,7 @@ const config = {
       copyright: `Copyright ${new Date().getFullYear()} Conholdate.`
     },
     prism: {
-      additionalLanguages: ["csharp", "powershell", "bash", "docker"]
+      additionalLanguages: ["csharp", "java", "powershell", "bash", "docker"]
     }
   }
 };
